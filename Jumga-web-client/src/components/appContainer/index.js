5ce96e3eea8d6245773940d1babe4cfa18845e0e
Chibuokem Jerry <profkiti@gmail.com>
2021-01-10 12:29:43 +0100

    initial setup with auth flow

--- Jumga-web-client/src/components/appContainer/index.js
+++ Jumga-web-client/src/components/appContainer/index.js
@@ -7,7 +7,7 @@ export default function AppContainer(props) {
 		<div className="App">
 			<img className="gift1" src={Gift1} alt="gift icon 1" />
 			<img className="gift2" src={Gift2} alt="gift icon 2" />
-			<div className="App-header-container scroll-bar">
+			<div className="App-header-container">
 				<header className="App-header scroll-bar">
 					{props.children}
 				</header>
